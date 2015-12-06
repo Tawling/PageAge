c116b80814d9cea7c70f24e58b6a54fb4d72558e
Tawling <zippydee111@gmail.com>
2015-12-06 03:23:03 -0800

    Popup fix
    Properly adds "temp" text to temporary enabled state after switching to
    enabled from page exception state

--- popup.js
+++ popup.js
@@ -66,6 +66,33 @@ function update(){
 	//
 	chrome.runtime.sendMessage({message: "popup-select",state:val},function(data){
 		//do the thing
+		switch(data){
+			case "enabled-temp":
+				$("#statusbar").text("PageAge is enabled (temporarily)");
+				$("#statusbar").css({"background-color":"#336600"});
+				$("#renabled").prop("checked",true);
+				break;
+			case "enabled":
+				$("#statusbar").text("PageAge is enabled");
+				$("#statusbar").css({"background-color":"#336600"});
+				$("#renabled").prop("checked",true);
+				break;
+			case "temp":
+				$("#statusbar").text("PageAge is disabled for this instance");
+				$("#statusbar").css({"background-color":"#808080"});
+				$("#rtemp").prop("checked",true);
+				break;
+			case "tab":
+				$("#statusbar").text("PageAge is disabled for this tab");
+				$("#statusbar").css({"background-color":"#0066cc"});
+				$("#rtab").prop("checked",true);
+				break;
+			case "page":
+				$("#statusbar").text("PageAge is disabled for this page");
+				$("#statusbar").css({"background-color":"#cc9900"});
+				$("#rpage").prop("checked",true);
+				break;
+		}
 		console.log("data");
 	});
 }
