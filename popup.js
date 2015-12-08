7028dd8df8872140c6dc99bdbeb247c856cfcd92
Tawling <zippydee111@gmail.com>
2015-12-07 17:56:16 -0800

    Popup is prettier

--- popup.js
+++ popup.js
@@ -39,31 +39,38 @@ function updateState(state, pageFlag){
 		//show that the current page is in the disabled list
 		//dotted background or something. I dunno.
 	}
+	$("#denabled").removeClass("checked");
+	$("#dtemp").removeClass("checked");
+	$("#dtab").removeClass("checked");
 	switch(state){
 		case "enabled-temp":
 			$("#statustext").text("PageAge is enabled (temporarily)");
-			$("#statusbar").css({"background-color":"#336600"});
+			//$("#statusbar").css({"background-color":"#336600"});
 			$("#renabled").prop("checked",true);
+			$("#denabled").addClass("checked");
 			break;
 		case "enabled":
 			$("#statustext").text("PageAge is enabled");
-			$("#statusbar").css({"background-color":"#336600"});
+			//$("#statusbar").css({"background-color":"#336600"});
 			$("#renabled").prop("checked",true);
+			$("#denabled").addClass("checked");
 			if (!pageFlag) $("#undospan").hide();
 			break;
 		case "temp":
 			$("#statustext").text("PageAge is disabled for this instance");
-			$("#statusbar").css({"background-color":"#808080"});
+			//$("#statusbar").css({"background-color":"#808080"});
 			$("#rtemp").prop("checked",true);
+			$("#dtemp").addClass("checked");
 			break;
 		case "tab":
 			$("#statustext").text("PageAge is disabled for this tab");
-			$("#statusbar").css({"background-color":"#0066cc"});
+			//$("#statusbar").css({"background-color":"#0066cc"});
 			$("#rtab").prop("checked",true);
+			$("#dtab").addClass("checked");
 			break;
 		case "page":
 			$("#statustext").text("PageAge is disabled for this site");
-			$("#statusbar").css({"background-color":"#cc9900"});
+			//$("#statusbar").css({"background-color":"#cc9900"});
 			$("#rpage").prop("checked",true);
 			$("#undospan").hide();
 			break;
